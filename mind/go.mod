c06d6a395dfa469572ffa98960da5b769b87df16
Joel Holmes <jholmes@ecr.ai>
2020-02-25 10:29:13 -0500

    init commit of cli

---
+++ mind/go.mod
@@ -0,0 +1,10 @@
+module github.com/Holmes89/alexandria/mind
+
+go 1.13
+
+require (
+	github.com/go-resty/resty/v2 v2.2.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v0.0.6
+	github.com/spf13/viper v1.6.2
+)
---
+++ mind/internal/app.go
@@ -0,0 +1,22 @@
+package internal
+
+import (
+	"errors"
+	"github.com/spf13/viper"
+)
+
+type App struct {
+	Endpoint string
+	Config *viper.Viper
+}
+
+
+func (app *App) SetConfigurationValue(key, value string) error {
+	switch key {
+	case "endpoint":
+		app.Config.Set("endpoint", value)
+	default:
+		return errors.New("invalid configuration setting")
+	}
+	return app.Config.WriteConfig()
+}
