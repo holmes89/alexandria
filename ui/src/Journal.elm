c45ad6a5e39fb0102104cb3d6b18481ec7beeb91
Joel Holmes <jholmes@ecr.ai>
2020-04-08 15:01:08 -0400

    working journal

--- ui/src/Journal.elm
+++ ui/src/Journal.elm
@@ -1,13 +1,16 @@
-module Journal exposing (Entry, entriesDecoder)
+module Journal exposing (Entry, entriesDecoder, entryDecoder, entryEncoder)
 
 import Json.Decode as Decode exposing (Decoder, field, string)
+import Json.Decode.Extra exposing (datetime)
 import Json.Decode.Pipeline exposing (optional, required)
+import Json.Encode as Encode
+import Time exposing (Posix)
 
 
 type alias Entry =
     { id : String
     , content : String
-    , created : String
+    , created : Posix
     }
 
 
@@ -16,9 +19,16 @@ entryDecoder =
     Decode.succeed Entry
         |> required "id" string
         |> required "content" string
-        |> required "created" string
+        |> required "created" datetime
 
 
 entriesDecoder : Decoder (List Entry)
 entriesDecoder =
     Decode.list entryDecoder
+
+
+entryEncoder : Entry -> Encode.Value
+entryEncoder entry =
+    Encode.object
+        [ ( "content", Encode.string entry.content )
+        ]
--- ui/src/Page/Journal.elm
+++ ui/src/Page/Journal.elm
@@ -1,10 +1,13 @@
 module Page.Journal exposing (Model, Msg, init, update, view)
 
 import Html exposing (..)
-import Html.Attributes exposing (class, href, id, rows, src, style)
+import Html.Attributes exposing (class, href, id, rows, src, style, value)
+import Html.Events exposing (onClick, onInput)
 import Http
-import Journal exposing (Entry, entriesDecoder)
+import Journal exposing (Entry, entriesDecoder, entryDecoder, entryEncoder)
 import Session exposing (..)
+import Time exposing (Month, Posix, toDay, toHour, toMinute, toMonth, toYear)
+import TimeZone exposing (america__new_york)
 
 
 
@@ -14,12 +17,14 @@ import Session exposing (..)
 type Status
     = Failure
     | Loading
-    | Success (List Entry)
+    | Success
 
 
 type alias Model =
     { token : Token
     , status : Status
+    , list : List Entry
+    , content : String
     }
 
 
@@ -27,6 +32,8 @@ init : Token -> ( Model, Cmd Msg )
 init token =
     ( { token = token
       , status = Loading
+      , list = []
+      , content = ""
       }
     , fetchEntries token
     )
@@ -38,7 +45,10 @@ init token =
 
 type Msg
     = FetchEntries (Result Http.Error (List Entry))
+    | AddEntry (Result Http.Error Entry)
     | Error
+    | UpdateContent String
+    | SendContent
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -46,14 +56,28 @@ update msg model =
     case msg of
         FetchEntries result ->
             case result of
-                Ok url ->
-                    ( { model | status = Success url }, Cmd.none )
+                Ok list ->
+                    ( { model | status = Success, list = list }, Cmd.none )
+
+                Err _ ->
+                    ( { model | status = Failure }, Cmd.none )
+
+        AddEntry result ->
+            case result of
+                Ok entry ->
+                    ( { model | status = Success, list = model.list ++ [ entry ], content = "" }, Cmd.none )
 
                 Err _ ->
                     ( { model | status = Failure }, Cmd.none )
 
         Error ->
             ( { model | status = Failure }, Cmd.none )
+
+        UpdateContent content ->
+            ( { model | content = content }, Cmd.none )
+
+        SendContent ->
+            ( model, createEntry model )
 
 
 
@@ -69,18 +93,18 @@ subscriptions model =
 -- VIEW
 
 
-view : Model -> Html msg
+view : Model -> Html Msg
 view model =
-    case model.status of
-        Failure ->
+    case ( model.status, model.list ) of
+        ( Failure, _ ) ->
             div []
                 [ text "Failed"
                 ]
 
-        Loading ->
+        ( Loading, _ ) ->
             text "Loading..."
 
-        Success entries ->
+        ( Success, entries ) ->
             section [ class "hero" ]
                 [ div [ class "hero-body journal-entry-area" ]
                     [ div [ class "container" ]
@@ -93,25 +117,86 @@ view model =
                     [ div [ class "container" ]
                         [ div [ class "columns" ]
                             [ div [ class "column is-11" ]
-                                [ textarea [ class "textarea", rows 5 ] [] ]
+                                [ textarea [ class "textarea", value model.content, onInput UpdateContent, rows 5 ] [] ]
                             , div [ class "column journal-entry-button" ]
-                                [ button [ class "button" ] [ text "Submit" ] ]
+                                [ button [ class "button", onClick SendContent ] [ text "Submit" ] ]
                             ]
                         ]
                     ]
                 ]
 
 
-viewEntry : Entry -> Html msg
+viewEntry : Entry -> Html Msg
 viewEntry entry =
     div [ class "columns" ]
-        [ div [ class "column is-1" ] [ text entry.created ]
+        [ div [ class "column is-1 has-text-right" ]
+            [ div [] [ text (formatDate entry.created) ]
+            , div [] [ text (formatTime entry.created) ]
+            ]
         , div [ class "column" ]
             [ article [ class "message is-dark", id entry.id ]
                 [ div [ class "message-body" ] [ text entry.content ]
                 ]
             ]
         ]
+
+
+formatDate : Posix -> String
+formatDate time =
+    let
+        zone =
+            america__new_york ()
+    in
+    monthToString (toMonth zone time) ++ "/" ++ String.fromInt (toDay zone time) ++ "/" ++ String.fromInt (toYear zone time)
+
+
+formatTime : Posix -> String
+formatTime time =
+    let
+        zone =
+            america__new_york ()
+    in
+    String.fromInt (toHour zone time) ++ ":" ++ String.fromInt (toMinute zone time)
+
+
+monthToString : Month -> String
+monthToString month =
+    case month of
+        Time.Jan ->
+            "1"
+
+        Time.Feb ->
+            "2"
+
+        Time.Mar ->
+            "3"
+
+        Time.Apr ->
+            "4"
+
+        Time.May ->
+            "5"
+
+        Time.Jun ->
+            "6"
+
+        Time.Jul ->
+            "7"
+
+        Time.Aug ->
+            "8"
+
+        Time.Sep ->
+            "9"
+
+        Time.Oct ->
+            "10"
+
+        Time.Nov ->
+            "11"
+
+        Time.Dec ->
+            "12"
 
 
 
@@ -125,6 +210,26 @@ fetchEntries token =
         , expect = Http.expectJson FetchEntries entriesDecoder
         , headers = [ Http.header "Authorization" token ]
         , method = "GET"
+        , timeout = Nothing
+        , tracker = Nothing
+        , url = "https://docs.jholmestech.com/journal/entry/"
+        }
+
+
+createEntry : Model -> Cmd Msg
+createEntry model =
+    let
+        entry =
+            { id = "", content = model.content, created = Time.millisToPosix 0 }
+
+        token =
+            model.token
+    in
+    Http.request
+        { body = Http.jsonBody <| entryEncoder entry
+        , expect = Http.expectJson AddEntry entryDecoder
+        , headers = [ Http.header "Authorization" token ]
+        , method = "POST"
         , timeout = Nothing
         , tracker = Nothing
         , url = "https://docs.jholmestech.com/journal/entry/"
